2a1fc4d6b4dc7d3648607cc3ab0d18180e676a70
Daniel Cuznetov <daniel.cuznetov@ebs-integrator.com>
2022-06-03 15:05:27 +0300

    Rename

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from setuptools import setup
 
-version = '0.0.1'
-
 setup(
-    packages=['azure_transcript'],
-    install_requires=['django'],
+    install_requires=['djangorestframework', 'django'],
 )
