75516b2aa01b2d38d6cfd756a31cf1487422b1d8
Daniel Cuznetov <daniel.cuznetov@ebs-integrator.com>
2022-06-03 14:34:28 +0300

    Redact setup

--- setup.py
+++ setup.py
@@ -3,11 +3,6 @@ from setuptools import setup
 version = '0.0.1'
 
 setup(
-    name='azure-transcript',
-    version=version,
-    description='A Python library for interacting with Azure Transcription API',
-    author='Daniel Cuznetov',
-    author_email='danielcuznetov04@gmail.com',
     packages=['azure_transcript'],
     install_requires=['django'],
 )
