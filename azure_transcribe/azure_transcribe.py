869697471cdda1a5d0307be9af5c9ee6e1b825f5
ion.bunduchi <ion.bunduchi@mail.ebs-integrator.com>
2022-06-07 11:46:58 +0300

    Defining the methods that create a transcribe job and return the result as text

--- azure_transcribe/azure_transcribe.py
+++ azure_transcribe/azure_transcribe.py
@@ -1,58 +1,72 @@
+import time
 from urllib.parse import urljoin
 from uuid import uuid4
 
 from datetime import datetime, timedelta
-from azure.storage.blob import BlobClient, generate_blob_sas, BlobSasPermissions, BlobServiceClient
+
+import requests
+from azure.storage.blob import generate_blob_sas, BlobSasPermissions, BlobServiceClient
 
 
 class BlobService:
-    def __init__(self, account_namae, account_key, container, blob, conn_str):
-        self.account_name = 'my2022storage'
-        self.account_key = 'Ni/DPFIpxm5D/5rpjuxhBlTT53MI7bVryKs6WkDXVuBEEM3bfRRpQcd1XlvTdOJVRRY8/HRW+nT4+ASt6VUSUQ=='
-        self.container_name = 'newcontainer'
-        self.blob_name = 'another_one.mp3'
-        self.connect_str = 'DefaultEndpointsProtocol=https;AccountName=my2022storage;AccountKey=Ni/DPFIpxm5D' \
-                           '/5rpjuxhBlTT53MI7bVryKs6WkDXVuBEEM3bfRRpQcd1XlvTdOJVRRY8/HRW+nT4+ASt6VUSUQ==;EndpointSuffix=core' \
-                           '.windows.net '
+    def __init__(self, account_name, account_key, container, conn_str):
+        self.account_name = account_name
+        self.account_key = account_key
+        self.container_name = container
+        self.connect_str = conn_str
         self.blob_service_client = BlobServiceClient.from_connection_string(self.connect_str)
 
     def post_blob(self, file_name):
         file = file_name
         blob_client = self.blob_service_client.get_blob_client(container=self.container_name, blob=file)
         with open(file, "rb") as data:
-            blob_client.upload_blob(data)
+            blob_client.upload_blob(data, overwrite=True)
+
+    def get_blob_sas(self, file_name):
+        sas_blob = generate_blob_sas(account_name=self.account_name,
+                                     container_name=self.container_name,
+                                     blob_name=file_name,
+                                     account_key=self.account_key,
+                                     permission=BlobSasPermissions(read=True),
+                                     expiry=datetime.utcnow() + timedelta(hours=3))
+        url = 'https://' + self.account_name + '.blob.core.windows.net/' + self.container_name + '/' + file_name + '?' + sas_blob
+        return url
 
 
 class AzureTranscribe:
     def __init__(self, base_url: str, token: str):
         self.base_url = base_url
         self.token = token
-        headers = {
+        self.headers = {
             "Content-Type": "application/json",
             "Ocp-Apim-Subscription-Key": self.token
         }
 
-    def create_transcription(self, file_url: str, language: str = 'en-US') -> dict:
+    def create_transcription(self, sas_url: str, language: str = 'en-US') -> dict:
         url = urljoin(self.base_url, 'transcriptions')
         payload = {
             "contentUrls": [
-                file_url
+                sas_url
             ],
             "locale": language,
             "displayName": f"{uuid4()}"
         }
-        return dict()
+        import json
+        response = requests.post(url, headers=self.headers, data=json.dumps(payload))
+        response.raise_for_status()
+        return response.json()['self']
 
+    def check_status(self, transcription_url):
+        for i in range(40):
+            response = requests.get(transcription_url, headers=self.headers)
+            if response.json()['status'] == 'Succeeded':
+                return response.json()['links']['files']
+            time.sleep(15)
+            if i == 39:
+                raise TimeoutError
 
-def get_blob_sas(account_name, account_key, container_name, blob_name):
-    sas_blob = generate_blob_sas(account_name=account_name,
-                                 container_name=container_name,
-                                 blob_name=blob_name,
-                                 account_key=account_key,
-                                 permission=BlobSasPermissions(read=True),
-                                 expiry=datetime.utcnow() + timedelta(hours=1))
-    return sas_blob
-
-
-blob = get_blob_sas(account_name, account_key, container_name, blob_name)
-url = 'https://' + account_name + '.blob.core.windows.net/' + container_name + '/' + blob_name + '?' + blob
+    def get_result(self, files_url):
+        response = requests.get(files_url, headers=self.headers)
+        file_url = response.json()['values'][1]['links']['contentUrl']
+        response = requests.get(file_url)
+        return response.json()['combinedRecognizedPhrases'][0]['display']
