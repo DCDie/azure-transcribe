4ff933b5b76201af95f2c3cf251911bc3951a137
Daniel Cuznetov <daniel.cuznetov@ebs-integrator.com>
2022-06-07 17:22:08 +0300

    Add packages

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(
-    packages=['azure_transcribe'],
+    packages=['azure_transcribe', 'azure_transcribe.blob', 'azure_transcribe.fixtures', 'azure_transcribe.transcribe'],
     install_requires=['azure-storage-blob'],
 )
